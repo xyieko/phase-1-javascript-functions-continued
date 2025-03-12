832fcc70cff9bf861c52aebbfb49816bec27dc57
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-03-12 18:09:19 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  const mondayWork = function(task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
+  };
+  
+  
+  console.log(saturdayFun());  
+  console.log(saturdayFun("bathe my dog"));  
+  
+  console.log(mondayWork());  
+  console.log(mondayWork("work from home"))
+  
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
